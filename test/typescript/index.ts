3775f728745b6066b584f606644c31bdb4bf0bf3
Bohdan L <bogdan.uz@gmail.com>
2016-04-10 02:40:24 +0300

    add base typescript definition test fix typescript definition

---
+++ test/typescript/index.ts
@@ -0,0 +1,27 @@
+import f from '../../index.d.ts';
+
+const filter1 = f()
+    .or(f().eq(x => x.toLower('name'), 'apple'))
+    .or(f().eq(x => x.toLower('name'), 'microsoft'))
+    .and(f().eq(x => x.toLower('type'), 'os'));
+
+const filter2 = f('or')
+    .eq(x => x.toLower('name'), 'apple')
+    .eq(x => x.toLower('name'), 'microsoft')
+    .and(x => x.ne('type', 'OS'));
+
+const filter3 = f('or')
+    .eq("tolower('name')", 'apple')
+    .eq("tolower('name')", 'microsoft')
+    .and("ne('type', 'OS')");
+
+const filter4 = f()
+    .ne('type', 'OS')
+    .and(f.or().eq('nama', 'Apple').eq('name', 'Microsoft'));
+
+const filter5 = f()
+    .ne('type', 'OS')
+    .in('name', ['Apple', 'Microsoft']);
+
+console.log(filter1.toString());
+
---
+++ test/typescript_spec.js
@@ -0,0 +1,17 @@
+/*eslint-env node, mocha */
+/*global describe, it*/
+import path from 'path';
+import {expect} from 'chai';
+import * as tt from 'typescript-definition-tester';
+
+describe('TypeScript definitions', function () {
+  this.timeout(0);
+
+  it('should compile against index.d.ts', (done) => {
+    tt.compileDirectory(
+        path.join(__dirname, 'typescript'),
+        fileName => fileName.match(/\.ts$/),
+        () => done()
+    );
+  });
+});
