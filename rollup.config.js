83b1774dc4d3b9ba6599fc128600447aa7082fd1
bohdan.lyzanets <bohdan.lyzanets@betlab.com>
2016-09-26 16:28:25 +0300

    replace babel-preset-es2015-rollup with babel-preset-es2015

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 export default {
   plugins: [
     babel({
-      presets: ['es2015-rollup'],
+      presets: [['es2015', { "modules": false }]],
       babelrc: false
     })
   ]
