6c8e514ff89537a3fe359c2bf176a7616e5debaa
Bohdan Lyzanets <bogdan.uz@gmail.com>
2017-12-15 02:11:12 +0200

    Bump dev dependencies & migrate to jest (#10)

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel';
 export default {
   plugins: [
     babel({
-      presets: [['es2015', { "modules": false }]],
+      presets: [['@babel/preset-env', { modules: false }]],
       babelrc: false
     })
   ]
